a2edfeb35c4fabc6af35c537a422f90065ac15b1
Dio Utoro <dioutoro@googlemail.com>
2026-06-25 07:07:19 +0200

    Finishing the SQL analysis

--- Superstore Dataset/sql/TimeTrend.sql
+++ Superstore Dataset/sql/TimeTrend.sql
@@ -1,11 +1,105 @@
 -- Time Trend Analysis --
 -- Calculate monthly sales trends.
+SELECT
+	EXTRACT(year FROM order_date) AS year,
+	EXTRACT(month FROM order_date) AS month,
+	SUM(sales) AS monthly_sales
+FROM customer
+GROUP BY year, month
+ORDER BY year, month;
 -- Calculate monthly profit trends.
+SELECT
+    EXTRACT(year FROM order_date) AS year,
+    EXTRACT(month FROM order_date) AS month,
+    SUM(profit) as monthly_profit
+FROM customer
+GROUP BY year, month
+ORDER BY year, month
 -- Find the best-selling product within each category.
+-- This is best-selling by total product sold
+with ranked_products AS (
+	SELECT
+	product_name,
+	category,
+	SUM(quantity) AS quantity_sold,
+	RANK() OVER(
+		PARTITION BY category
+		ORDER BY SUM(quantity)
+	) AS product_rank
+	FROM customer
+	GROUP BY product_name, category
+)
+SELECT *
+	FROM ranked_products
+WHERE product_rank = 1; 
+-- This is best-selling by total revenue
+with ranked_sales AS (
+	SELECT
+	product_name,
+	category,
+	SUM(sales) AS total_sales,
+	RANK() OVER(
+		PARTITION BY category
+		ORDER BY SUM(sales)
+	) AS sales_rank
+	FROM customer
+	GROUP BY product_name, category
+)
+SELECT *
+	FROM ranked_sales
+WHERE sales_rank = 1;
+-- Now best-selling by profit
+with ranked_profit AS(
+	SELECT
+	product_name,
+	category,
+	SUM(profit) AS total_profit,
+	RANK() OVER(
+		PARTITION BY category
+		ORDER BY SUM(profit) DESC
+	) AS profit_rank
+	FROM customer
+	GROUP BY product_name, category
+)
+SELECT *
+	FROM ranked_profit
+WHERE profit_rank = 1;
 -- Rank states by total sales.
+SELECT
+	state,
+	RANK() OVER(ORDER BY SUM(sales)DESC)
+FROM customer
+GROUP BY state;
 -- Rank products by profit within each category.
--- Calculate each region's contribution to total company sales.
--- Identify products whose sales are above the company average.
--- Compare profit margins across categories.
+with ranked_profit AS(
+	SELECT
+	product_name,
+	category,
+	SUM(profit) AS total_profit,
+	RANK() OVER(
+		PARTITION BY category
+		ORDER BY SUM(profit) DESC
+	) AS profit_rank
+	FROM customer
+	GROUP BY product_name, category
+)
+SELECT *
+	FROM ranked_profit
+WHERE profit_rank = 1;
 -- Find the top-performing category in each region.
+SELECT
+	category,
+	region,
+	SUM(sales) AS total_sales,
+	RANK() OVER(PARTITION BY region ORDER BY SUM(sales)DESC)
+	FROM customer
+GROUP BY category, region;
 -- Identify customers who generate the highest lifetime sales
+SELECT
+	customer_id,
+	customer_name,
+	SUM(sales) total_sales,
+	RANK() OVER(ORDER BY SUM(sales)DESC)
+	FROM customer
+GROUP BY customer_id, customer_name
+LIMIT 1;
--- Superstore Dataset/sql/TopN.sql
+++ Superstore Dataset/sql/TopN.sql
@@ -34,7 +34,43 @@ FROM customer
 GROUP BY "sub-category"
 HAVING SUM(profit) < 0;
 -- Calculate sales and profit by customer segment.
+SELECT segment,
+SUM(sales) AS total_sales
+FROM customer
+GROUP BY segment
+ORDER BY total_sales DESC;
+
+SELECT segment,
+SUM(profit) AS total_profit
+FROM customer
+GROUP BY segment
+ORDER BY total_profit DESC;
 -- Calculate sales and profit by shipping mode.
+SELECT ship_mode,
+SUM(sales) AS total_sales
+FROM customer
+GROUP BY ship_mode
+ORDER BY total_sales DESC;
+SELECT ship_mode,
+SUM(profit) AS total_profit
+FROM customer
+GROUP BY ship_mode
+ORDER BY total_profit DESC;
 -- Find the average discount by category.
+SELECT category,
+AVG(discount) AS avg_discount_category
+FROM customer
+GROUP BY category
+ORDER BY avg_discount_category;
 -- Determine which region has the highest profit margin.
+SELECT region,
+SUM(profit) AS total_profit
+FROM customer
+GROUP BY region
+ORDER BY total_profit DESC;
 -- Find products that have generated losses overall.
+SELECT product_name,profit AS profit
+FROM customer
+WHERE profit < 0
+ORDER BY profit ASC
+LIMIT 10;

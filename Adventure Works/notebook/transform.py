34b9e686b28db9bcba36c473faf7cde01e46244f
Dio Utoro <dioutoro@googlemail.com>
2026-07-08 11:27:54 +0200

    Add string to date conversion in clean_table()

--- Adventure Works/notebook/transform.py
+++ Adventure Works/notebook/transform.py
@@ -1,9 +1,14 @@
+import pandas as pd
 # Check data profile
 def table_profile(df, table_name):
-    print(f"\nTable: {table_name}")
+    print(f"\nTable: {table_name.upper()}")
     
     # Shape
     print(f"Shape: {df.shape}")
+
+    # Columns
+    print("\nColumn(s):")
+    print(df.columns.tolist())
 
     # Null-values
     print("\nMissing Values:")
@@ -14,7 +19,11 @@ def table_profile(df, table_name):
     print(df.dtypes)
 
 # Clean data
-def clean_data(df, table_name):
-    df = df.drop_duplicates()
+def clean_table(df_raw, table_name):
+    df_raw = df_raw.drop_duplicates()
+    for cols in df_raw.columns:
+        if 'date' in cols.lower():
+            df_raw[cols] = pd.to_datetime(df_raw[cols], errors='coerce')
+    df_cleaned = df_raw.dropna()
+    return df_cleaned
 
-    return df
